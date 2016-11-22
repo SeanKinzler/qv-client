3db9823cf02491c9b0d9c050022ad52f121893ad
seankinzler <sean.kinzler92@gmail.com>
2016-11-21 20:10:26 -0800

    converted to https

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const logger = require('./logger');
+const fs = require('fs');
+const path = require('path');
 
 const argv = require('minimist')(process.argv.slice(2));
 const setup = require('./middlewares/frontendMiddleware');
@@ -9,6 +11,13 @@ const isDev = process.env.NODE_ENV !== 'production';
 const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false;
 const resolve = require('path').resolve;
 const app = express();
+
+const serverConfig = {
+  key: fs.readFileSync(path.join(__dirname, './credentials/key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, './credentials/cert.pem')),
+  // ca: fs.readFileSync(path.join(__dirname, './credentials/chain.pem'))
+};
+
 
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 // app.use('/api', myApi);
@@ -22,8 +31,9 @@ setup(app, {
 // get the intended port number, use port 3000 if not provided
 const port = argv.port || process.env.PORT || 3000;
 
+const server = require('https').createServer(serverConfig, app);
 // Start your app.
-app.listen(port, (err) => {
+server.listen(port, (err) => {
   if (err) {
     return logger.error(err.message);
   }
